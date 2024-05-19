d3ff17752853f7006be619f24dc4d6ac14851503
Bintngpr <adrielle.422023021@civitas.ukrida.ac.id>
2024-05-19 19:45:27 +0700

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *      version="1.0",
+ *      title="My Rest API",
+ *      description="Laravel project to practice developing Rest API with L5 Swagger OpenAPI",
+ *      x={
+ *          "logo": {
+ *              "url": "https://miro.medium.com/v2/resize:fit:1200/1*J3G3akaMpUOLegw0p0qthA.png"
+ *          }
+ *      },
+ *      @OA\Contact(
+ *          name="Adrielle Bintang",
+ *          email="adrielle.422023021@civitas.ukrida.ac.id"
+ *      ),
+ * )
+ */
 abstract class Controller
 {
-    //
+    
 }
